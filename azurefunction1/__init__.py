b3e5f83a960622daf6e4d1e306d9fe4e2cad1b01
meluzyn100 <255939@student.pwr.edu.pl>
2022-10-22 23:05:09 +0200

    test

--- azurefunction1/__init__.py
+++ azurefunction1/__init__.py
@@ -18,7 +18,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
-        open("hello.txt", "w")
+        f = open("hello.txt", "r")
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
