727979bfcddf64a76cb63fc64366c089142d27d2
meluzyn100 <255939@student.pwr.edu.pl>
2022-12-17 18:41:52 +0100

    big changes

---
+++ py/bernstein_race/power/ILEBR.py
@@ -0,0 +1,30 @@
+import sys
+import numpy as np
+from mpire import WorkerPool
+from time import time
+sys.path.append('..')
+from cython_files.ILEBR.ILEBR import ILEBR_test
+
+s = time()
+with WorkerPool(n_jobs=4) as pool:
+    # ps1 = np.arange(0.495, 0.5+0.0005, 0.0005)
+    # x1_ILEBR = np.array([pool.map(ILEBR_test, [p]*2000) for p in ps1])
+    # np.save("powers/ILEBR_powr", x1_ILEBR)
+
+    # ps2 = np.arange(0, 0.5+0.05, 0.05)
+    # x2_ILEBR = np.array([pool.map(ILEBR_test, [p]*200) for p in ps2])
+    # np.save("nr_of_games_to_play/ILEBR_t", x2_ILEBR)
+
+    ps1 = np.arange(0, 0.5, 0.1)
+    ps2 = np.arange(0, 0.5, 0.1)
+    x1_ILEBR = []
+    for p in ps1:
+        x1_ILEBR.append(pool.map(ILEBR_test, [p]*1, progress_bar=True))
+    x1_ILEBR = np.array(x1_ILEBR)
+    np.save("powers/ILEBR_powr", x1_ILEBR)
+    x2_ILEBR = []
+    for p in ps2:
+        x2_ILEBR.append(pool.map(ILEBR_test, [p]*10))
+    x2_ILEBR = np.array(x2_ILEBR)
+    np.save("nr_of_games_to_play/ILEBR_t", x2_ILEBR)
+print("ILEBR total time: ", time() - s)
---
+++ py/bernstein_race/power/ILEBR_star.py
@@ -0,0 +1,30 @@
+import sys
+import numpy as np
+from mpire import WorkerPool
+from time import time
+sys.path.append('..')
+from cython_files.ILEBR_star.ILEBR_star import ILEBR_star_test
+
+s = time()
+with WorkerPool(n_jobs=4) as pool:
+    # ps1 = np.arange(0.495, 0.5+0.0005, 0.0005)
+    # x1_ILEBR_star = np.array([pool.map(ILEBR_star_test, [p]*2000) for p in ps1])
+    # np.save("powers/ILEBR_star_powr", x1_ILEBR_star)
+
+    # ps2 = np.arange(0, 0.5+0.05, 0.05)
+    # x2_ILEBR_star = np.array([pool.map(ILEBR_star_test, [p]*200) for p in ps2])
+    # np.save("nr_of_games_to_play/ILEBR_star_t", x2_ILEBR_star)
+
+    ps1 = np.arange(0, 0.5, 0.1)
+    ps2 = np.arange(0, 0.5, 0.1)
+    x1_ILEBR_star = []
+    for p in ps1:
+        x1_ILEBR_star.append(pool.map(ILEBR_star_test, [p]*1, progress_bar=True))
+    x1_ILEBR_star = np.array(x1_ILEBR_star)
+    np.save("powers/ILEBR_star_powr", x1_ILEBR_star)
+    x2_ILEBR_star = []
+    for p in ps2:
+        x2_ILEBR_star.append(pool.map(ILEBR_star_test, [p]*10))
+    x2_ILEBR_star = np.array(x2_ILEBR_star)
+    np.save("nr_of_games_to_play/ILEBR_star_t", x2_ILEBR_star)
+print("ILEBR_star total time: ", time() - s)
